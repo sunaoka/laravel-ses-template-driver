8e6949f7a16087b7a3b4d28f90ef2a88d7318f07
sunaoka <sunaoka@pocari.org>
2020-06-30 14:59:50 +0900

    revert 64fe0b9b12810fb8b18b2fa280d6596372325076

--- tests/SesTemplateTransportServiceProviderTest.php
+++ tests/SesTemplateTransportServiceProviderTest.php
@@ -3,12 +3,9 @@
 namespace Sunaoka\LaravelSesTemplateDriver\Tests;
 
 use Aws\Ses\SesClient;
-use Illuminate\Config\Repository;
 use Illuminate\Container\Container;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Mail\Mailer;
 use Illuminate\Mail\MailServiceProvider;
-use Illuminate\Mail\MailManager;
+use Illuminate\Mail\TransportManager;
 use Illuminate\Support\Collection;
 use Sunaoka\LaravelSesTemplateDriver\SesTemplateTransportServiceProvider;
 use Sunaoka\LaravelSesTemplateDriver\Transport\SesTemplateTransport;
@@ -21,99 +18,46 @@ class SesTemplateTransportServiceProviderTest extends TestCase
     public function testRegister()
     {
         $app = new Container();
-        $app->singleton('config', function () {
-            return new Repository([
-                'mail.default' => 'sestemplate',
-                'mail.mailers.sestemplate' => ['transport' => 'sestemplate'],
-                'services' => [
-                    'ses' => [
-                        'key'    => 'foo',
-                        'secret' => 'bar',
-                        'region' => 'us-east-1',
-                    ],
-                ],
-            ]);
-        });
+        $app['config'] = new Collection([
+            'mail.driver'  => 'ses.template',
+            'services.ses' => [
+                'key'    => 'foo',
+                'secret' => 'bar',
+                'region' => 'us-east-1',
+            ],
+        ]);
 
         (new SesTemplateTransportServiceProvider($app))->register();
-        $this->callRestrictedMethod(new MailServiceProvider($app), 'registerIlluminateMailer');
+        $this->callRestrictedMethod(new MailServiceProvider($app), 'registerSwiftTransport');
 
-        $this->assertInstanceOf(MailManager::class, $app['mail.manager']);
+        $this->assertInstanceOf(TransportManager::class, $app['swift.transport']);
     }
 
     public function testRegisterDriver()
     {
         $app = new Container();
-        $app->singleton('config', function () {
-            return new Repository([
-                'mail.default' => 'sestemplate',
-                'mail.mailers.sestemplate' => ['transport' => 'sestemplate'],
-                'services' => [
-                    'ses' => [
-                        'key'    => 'foo',
-                        'secret' => 'bar',
-                        'region' => 'us-east-1',
-                    ],
-                ],
-            ]);
-        });
+        $app['config'] = new Collection([
+            'mail.driver'  => 'ses.template',
+            'services.ses' => [
+                'key'    => 'foo',
+                'secret' => 'bar',
+                'region' => 'us-east-1',
+            ],
+        ]);
 
-        $app->singleton('view', function () {
-            return new MockViewFacatory();
-        });
-
-        $app->singleton('events', function () {
-            return null;
-        });
-
-        $manager = new MailManager($app);
+        $manager = new TransportManager($app);
 
         $provider = new SesTemplateTransportServiceProvider($app);
         $provider->registerTransport($manager);
 
         /** @var SesTemplateTransport $transport */
-        $transport = $manager->getSwiftMailer()->getTransport();
+        $transport = $manager->driver();
+
         $this->assertInstanceOf(SesTemplateTransport::class, $transport);
 
         /** @var SesClient $ses */
         $ses = $transport->ses();
 
         $this->assertEquals('us-east-1', $ses->getRegion());
-    }
-
-}
-
-class MockViewFacatory implements \Illuminate\Contracts\View\Factory
-{
-    public function exists($view)
-    {
-    }
-
-    public function file($path, $data = [], $mergeData = [])
-    {
-    }
-
-    public function make($view, $data = [], $mergeData = [])
-    {
-    }
-
-    public function share($key, $value = null)
-    {
-    }
-
-    public function composer($views, $callback)
-    {
-    }
-
-    public function creator($views, $callback)
-    {
-    }
-
-    public function addNamespace($namespace, $hints)
-    {
-    }
-
-    public function replaceNamespace($namespace, $hints)
-    {
     }
 }
