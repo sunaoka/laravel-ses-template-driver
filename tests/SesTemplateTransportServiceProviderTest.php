0ba12d6b0a7ac8db539dbb3f9f537388be8f6651
sunaoka <sunaoka@pocari.org>
2020-06-30 15:22:38 +0900

    update SesTemplateTransportServiceProviderTest

--- tests/SesTemplateTransportServiceProviderTest.php
+++ tests/SesTemplateTransportServiceProviderTest.php
@@ -3,10 +3,10 @@
 namespace Sunaoka\LaravelSesTemplateDriver\Tests;
 
 use Aws\Ses\SesClient;
+use Illuminate\Config\Repository;
 use Illuminate\Container\Container;
 use Illuminate\Mail\MailServiceProvider;
 use Illuminate\Mail\TransportManager;
-use Illuminate\Support\Collection;
 use Sunaoka\LaravelSesTemplateDriver\SesTemplateTransportServiceProvider;
 use Sunaoka\LaravelSesTemplateDriver\Transport\SesTemplateTransport;
 
@@ -18,14 +18,16 @@ class SesTemplateTransportServiceProviderTest extends TestCase
     public function testRegister()
     {
         $app = new Container();
-        $app['config'] = new Collection([
-            'mail.driver'  => 'ses.template',
-            'services.ses' => [
-                'key'    => 'foo',
-                'secret' => 'bar',
-                'region' => 'us-east-1',
-            ],
-        ]);
+        $app->singleton('config', function () {
+            return new Repository([
+                'mail.driver'  => 'ses.template',
+                'services.ses' => [
+                    'key'    => 'foo',
+                    'secret' => 'bar',
+                    'region' => 'us-east-1',
+                ],
+            ]);
+        });
 
         (new SesTemplateTransportServiceProvider($app))->register();
         $this->callRestrictedMethod(new MailServiceProvider($app), 'registerSwiftTransport');
@@ -36,14 +38,16 @@ class SesTemplateTransportServiceProviderTest extends TestCase
     public function testRegisterDriver()
     {
         $app = new Container();
-        $app['config'] = new Collection([
-            'mail.driver'  => 'ses.template',
-            'services.ses' => [
-                'key'    => 'foo',
-                'secret' => 'bar',
-                'region' => 'us-east-1',
-            ],
-        ]);
+        $app->singleton('config', function () {
+            return new Repository([
+                'mail.driver'  => 'ses.template',
+                'services.ses' => [
+                    'key'    => 'foo',
+                    'secret' => 'bar',
+                    'region' => 'us-east-1',
+                ],
+            ]);
+        });
 
         $manager = new TransportManager($app);
 
